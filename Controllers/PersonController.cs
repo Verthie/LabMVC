afd2e746cd3a0c4dcd56222f050959b056501907
Verth <vertisixs@gmail.com>
2022-12-14 20:59:56 +0100

    Lab7

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVC_LAB.Models.Person;
+
+namespace WebAPImvc.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var model = new PersonViewModel()
+            {
+                Persons = new PersonModel().GetPersons()
+            };
+            return View(model);
+        }
+    }
+}
---
+++ Models/Person/GenderEnum.cs
@@ -0,0 +1,8 @@
+namespace MVC_LAB.Models
+{
+    public enum GenderEnum
+    {
+        Male = 1,
+        Female = 2
+    }
+}
